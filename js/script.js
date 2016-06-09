8c1efce32f81ffc77b68ea625b30620d4641b37a
Ramzi Kanazi <arcan3xx>
2016-06-09 16:23:50 -0400

    adding to raise issue and ask question re counter and columns

--- js/script.js
+++ js/script.js
@@ -1,9 +1,40 @@
 $(document).ready(function() {
 console.log('loaded');
 
-$('.col1').on('click', function(event) {
-  $(this).toggleClass('black');
-})
+// var played = .blue || .black
+// var current = 0;
+var counter = 0;
+
+// $('table').click(function() {
+//   counter ++;
+// });
+
+if ( counter % 2 === 0 ) {
+  $('.circle').on('click', function(event) {
+    counter++;
+    $(this).removeClass('blue').addClass('black');
+  })
+} else {
+  counter++;
+  $('.circle').on('click', function(event) {
+    $(this).removeClass('black').addClass('blue');
+  });
+};
+
+
+// $('div').hasClass('black', function(ev){
+//     $(this).on('click', function(event) {
+//       $(this).removeClass('black').addClass('blue');
+//   })
+// })
+
+// $('button').on('click', function(event) {
+//   $('.circle').removeClass(played);
+// });
+
+
+
+
 
 
 
